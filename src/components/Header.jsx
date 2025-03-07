0efc218c27d40db265a9d3a634879d414e1ee552
AlanBatres1 <alanbatres328@gmail.com>
2025-03-07 15:19:41 -0800

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export default function Header() {
+export default function div() {
   const [darkMode, setDarkMode] = useState(false)
 
   const toggleTheme = () => {
@@ -17,7 +17,7 @@ export default function Header() {
   
   return (
     <>
-      <header className="relative z-10 max-w-7xl mx-auto flex items-center flex-col md:flex-row justify-between ">
+      <div className="relative z-10 max-w-7xl mx-auto flex items-center flex-col md:flex-row justify-between ">
         <div>
           <h1 className="font-bold text-2xl text-slate-800 dark:text-white">
             Social Media Dashboard
@@ -34,7 +34,7 @@ export default function Header() {
             <div className="ball"></div>
           </label>
         </div>
-      </header>
+      </div>
     </>
   );
 }
