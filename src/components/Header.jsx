e1fb925e04f7f76431ca471352e9d86c82c41cb8
AlanBatres1 <alanbatres328@gmail.com>
2025-03-07 15:25:21 -0800

    sdfs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,40 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-export default function Header() {
-  const [darkMode, setDarkMode] = useState(false)
+// export default function Header() {
+//   const [darkMode, setDarkMode] = useState(false)
 
-  const toggleTheme = () => {
-    // const body = document.querySelector("body")
+//   const toggleTheme = () => {
+//     // const body = document.querySelector("body")
 
-    // if (!darkMode) {
-    //   body.classList.add("dark")
-    // } else {
-    //   body.classList.remove("dark")
-    // }
+//     // if (!darkMode) {
+//     //   body.classList.add("dark")
+//     // } else {
+//     //   body.classList.remove("dark")
+//     // }
 
-    // setDarkMode(!darkMode)
-  }
+//     // setDarkMode(!darkMode)
+//   }
   
-  return (
+//   return (
   
-      <div className="relative z-10 max-w-7xl mx-auto flex items-center flex-col md:flex-row justify-between ">
-        <div>
-          <h1 className="font-bold text-2xl text-slate-800 dark:text-white">
-            Social Media Dashboard
-          </h1>
-          <p className="text-slate-600 font-bold dark:text-slate-400">
-            Total Followers: 23,004
-          </p>
-        </div>
+//       <div className="relative z-10 max-w-7xl mx-auto flex items-center flex-col md:flex-row justify-between ">
+//         <div>
+//           <h1 className="font-bold text-2xl text-slate-800 dark:text-white">
+//             Social Media Dashboard
+//           </h1>
+//           <p className="text-slate-600 font-bold dark:text-slate-400">
+//             Total Followers: 23,004
+//           </p>
+//         </div>
 
-        {/* Toggle */}
-        <div className="toggle gap-[105px] md:gap-0 "><label htmlFor="checkbox" className="font-bold text-slate-600 dark:text-slate-400 text-sm cursor-pointer">Dark Mode</label>
-          <input type="checkbox" className="checkbox" id="checkbox" checked={darkMode} onChange={toggleTheme} />
-          <label htmlFor="checkbox" className="label">
-            <div className="ball"></div>
-          </label>
-        </div>
-      </div>
+//         {/* Toggle */}
+//         <div className="toggle gap-[105px] md:gap-0 "><label htmlFor="checkbox" className="font-bold text-slate-600 dark:text-slate-400 text-sm cursor-pointer">Dark Mode</label>
+//           <input type="checkbox" className="checkbox" id="checkbox" checked={darkMode} onChange={toggleTheme} />
+//           <label htmlFor="checkbox" className="label">
+//             <div className="ball"></div>
+//           </label>
+//         </div>
+//       </div>
     
-  );
-}
+//   );
+// }
